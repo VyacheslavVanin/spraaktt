efb4adc772546465257149cce7878df7b27dc093
Vyacheslav.Vanin <vyacheslav.vanin@gmail.com>
2026-01-31 12:10:12 +0300

    Add command line arguments

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import time
 import sounddevice as sd
 import numpy as np
 import sys
+import argparse
+import os
 
 
 class Transcriber:
@@ -164,8 +166,51 @@ class SpeachRecognizer:
         return transcription
 
 
+def parse_arguments():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Speech recognition and transcription tool"
+    )
+    parser.add_argument(
+        "--language",
+        "-l",
+        type=str,
+        help="Input language hint for transcription (e.g., 'en', 'no', 'de')",
+    )
+    parser.add_argument(
+        "--translate", "-t", action="store_true", help="Enable translation to English"
+    )
+    parser.add_argument(
+        "--stdout-file", type=str, help="File path to redirect stdout messages"
+    )
+    parser.add_argument(
+        "--stderr-file", type=str, help="File path to redirect stderr messages"
+    )
+
+    return parser.parse_args()
+
+
 def main():
-    speech_recognizer = SpeachRecognizer()
+    args = parse_arguments()
+
+    # Redirect stdout and stderr if file paths are provided
+    if args.stdout_file:
+        stdout_fd = os.open(
+            args.stdout_file, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o644
+        )
+        os.dup2(stdout_fd, sys.stdout.fileno())
+        os.close(stdout_fd)
+
+    if args.stderr_file:
+        stderr_fd = os.open(
+            args.stderr_file, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, 0o644
+        )
+        os.dup2(stderr_fd, sys.stderr.fileno())
+        os.close(stderr_fd)
+
+    speech_recognizer = SpeachRecognizer(
+        language_hint=args.language, translate=args.translate
+    )
 
     def start_listen():
         speech_recognizer.listen()
